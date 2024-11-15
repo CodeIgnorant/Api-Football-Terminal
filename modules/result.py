910e47c191caad5b66a4725a1eb2ffa3af7bebe5
Kemal Yilmaz <kemalylmz@gmail.com>
2024-11-15 23:20:14 +0300

    home draw away = 1 0 2

--- modules/result.py
+++ modules/result.py
@@ -1,26 +1,24 @@
-# result.py
-
 def calculate_match_result(df):
     """
     Calculate the match results based on the fulltime, halftime, and second half scores.
-    Adds 'Fulltime Result', 'Halftime Result', and 'Secondhalf Result' columns to the DataFrame with values 'home', 'away', or 'draw'.
+    Adds 'Fulltime Result', 'Halftime Result', and 'Secondhalf Result' columns to the DataFrame with values 1, 0, or 2.
     
     :param df: DataFrame containing fixture data.
     :return: DataFrame with new 'Fulltime Result', 'Halftime Result', and 'Secondhalf Result' columns.
     """
-    # Calculate Match Result
-    df['Fulltime Result'] = df.apply(lambda row: 'home' if row['Fulltime Home Score'] > row['Fulltime Away Score'] 
-                                             else 'away' if row['Fulltime Away Score'] > row['Fulltime Home Score']
-                                             else 'draw', axis=1)
+    # Calculate Match Result (Fulltime Result)
+    df['Fulltime Result'] = df.apply(lambda row: 1 if row['Fulltime Home Score'] > row['Fulltime Away Score'] 
+                                             else 2 if row['Fulltime Away Score'] > row['Fulltime Home Score']
+                                             else 0, axis=1)
     
     # Calculate Halftime Result
-    df['Halftime Result'] = df.apply(lambda row: 'home' if row['Halftime Home Score'] > row['Halftime Away Score'] 
-                                               else 'away' if row['Halftime Away Score'] > row['Halftime Home Score']
-                                               else 'draw', axis=1)
+    df['Halftime Result'] = df.apply(lambda row: 1 if row['Halftime Home Score'] > row['Halftime Away Score'] 
+                                               else 2 if row['Halftime Away Score'] > row['Halftime Home Score']
+                                               else 0, axis=1)
     
     # Calculate Secondhalf Result
-    df['Secondhalf Result'] = df.apply(lambda row: 'home' if row['Secondhalf Home Score'] > row['Secondhalf Away Score'] 
-                                                 else 'away' if row['Secondhalf Away Score'] > row['Secondhalf Home Score']
-                                                 else 'draw', axis=1)
+    df['Secondhalf Result'] = df.apply(lambda row: 1 if row['Secondhalf Home Score'] > row['Secondhalf Away Score'] 
+                                                 else 2 if row['Secondhalf Away Score'] > row['Secondhalf Home Score']
+                                                 else 0, axis=1)
     
     return df
